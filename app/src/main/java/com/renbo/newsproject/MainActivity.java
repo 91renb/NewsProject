9856e6d6b08fda5a03ad0b50596ae024d4001f28
任波 <borenfocus@gmail.com>
2019-05-17 19:08:48 +0800

    添加底部导航tabbar

--- app/src/main/java/com/renbo/newsproject/MainActivity.java
+++ app/src/main/java/com/renbo/newsproject/MainActivity.java
@@ -1,7 +1,15 @@
 package com.renbo.newsproject;
 
+import android.content.Context;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TabHost;
+
+import com.renbo.newsproject.tab.fragment.EmptyFragment;
+import com.renbo.newsproject.tab.fragment.HomeFragment;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 1.找到 FragmentTabHost
+        FragmentTabHost tabHost = (FragmentTabHost)findViewById(R.id.tab_host);
+        // 2.设置绑定fragment显示的容器（containerId：是将要显示的Fragment容器）
+        tabHost.setup(this, getSupportFragmentManager(), R.id.tab_container);
+        // 3.添加选项卡（tag 即是选项卡的 tabId）
+        TabHost.TabSpec one = tabHost.newTabSpec("0").setIndicator(getItemView(this));
+        tabHost.addTab(one, HomeFragment.class, null);
 
+        TabHost.TabSpec two = tabHost.newTabSpec("1").setIndicator(getItemView(this));
+        tabHost.addTab(two, EmptyFragment.class, null);
+
+        TabHost.TabSpec three = tabHost.newTabSpec("2").setIndicator(getItemView(this));
+        tabHost.addTab(three, EmptyFragment.class, null);
+
+
+        // 4.监听切换
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+
+            }
+        });
     }
 
+    public View getItemView(Context context) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View title_view = inflater.inflate(R.layout.view_item, null);
+        return title_view;
+    }
 }
---
+++ app/src/main/java/com/renbo/newsproject/tab/fragment/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.renbo.newsproject.tab.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.renbo.newsproject.R;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // 初始化fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
